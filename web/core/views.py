5bea95718fc07edace9072c08eac94a2b87f27ab
dachenlian <cclemon.lian@gmail.com>
2018-12-01 11:59:31 +0800

    added index

--- web/core/views.py
+++ web/core/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import UpdateView
+from core.models import Entry
 
-# Create your views here.
+
+class SearchListView(ListView):
+
+    model = Entry
+    paginate_by = 100
+    context_object_name = 'entries'
+    template_name = 'core/index.html'
+    ordering = ['-time']
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['total'] = Entry.objects.all().count()
+        return context
+
+
+class EntryUpdate(UpdateView):
+    model = Entry
+    fields = ['itemName', 'wordRoot', 'variant', 'toda', 'truku',
+              'isRoot', 'isPlant', 'meaning', 'meaningEn',
+              'sentence', 'sentenceCh', 'sentenceEn', 'wordClass',
+              ]
