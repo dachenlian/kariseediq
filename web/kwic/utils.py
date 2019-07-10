b29146bc0c38f3df2861cab10f17411a04578022
richl <cclemon.lian@gmail.com>
2019-07-10 23:38:29 +0800

    fixed punctuation

--- web/kwic/utils.py
+++ web/kwic/utils.py
@@ -27,12 +27,14 @@ def _add_variant(words: GeneratorType) -> list:
 
 
 def build_kwic(include_examples=False):
-    pat = r"(\w+)([{}])".format(string.punctuation)
+    pat_one = r"(\w+)([{}])".format(string.punctuation)
+    pat_two = r"([{}])(\w+)".format(string.punctuation)
 
     files = TextFile.objects.all()
     texts = " ".join(text.read_and_decode() for text in files)
     texts = re.sub('\n', ' ', texts).strip()
-    texts = re.sub(pat, r'\1 \2', texts)
+    texts = re.sub(pat_one, r'\1 \2', texts)
+    texts = re.sub(pat_two, r'\1 \2', texts)
     word_gen = (word for word in texts.split())
     words = _add_variant(word_gen)
     return Text(words)
