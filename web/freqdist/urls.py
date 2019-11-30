b8e5ff40ec460c45e235039b3d41ffcb8a3b84ba
richl <cclemon.lian@gmail.com>
2019-11-30 20:56:01 +0800

    make simple upload

--- web/freqdist/urls.py
+++ web/freqdist/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'freqdist'
 
 urlpatterns = [
-    path('upload-page/', views.TextFileUploadView.as_view(), name='upload'),
+    path('upload/', views.TextFileUploadView.as_view(), name='upload'),
     path('<int:pk>/delete', views.TextSingleDeleteView.as_view(), name='delete_single'),
     path('delete-all/', views.TextAllDeleteView.as_view(), name='delete_all'),
     path('results/', views.FreqResultsView.as_view(), name='results'),
