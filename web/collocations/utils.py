e479ea8387a87079067d7e82d8119348e07f25c1
dachenlian <cclemon.lian@gmail.com>
2019-12-21 00:55:49 +0800

    add window_size

--- web/collocations/utils.py
+++ web/collocations/utils.py
@@ -1,3 +1,4 @@
+import logging
 import nltk
 from nltk import collocations
 
@@ -6,6 +7,9 @@ from typing import List, Tuple
 from freqdist.models import TextFile
 from kwic.utils import clean_texts, get_texts
 from core.models import Example
+
+
+logger = logging.getLogger(__name__)
 
 
 MEASURES_FINDERS_DICT = {
@@ -29,17 +33,19 @@ def get_collocates(ngram: str,
                    include_examples: bool,
                    query: str = None,
                    freq_filter: int = 3,
-                   limit: int = 500) -> List[Tuple[str, str, int]]:
+                   window_size: int = None,
+                   limit: int = 1000) -> List[Tuple[str, str, int]]:
     measures, finder = MEASURES_FINDERS_DICT.get(ngram)
     texts = get_texts(include_examples)
     texts = clean_texts(texts)
     tokens = texts.split()
-    finder = finder.from_words(tokens)
+    finder = finder.from_words(tokens, window_size=window_size)
     if freq_filter:
         finder.apply_freq_filter(freq_filter)
     results = finder.score_ngrams(getattr(measures, assoc_measure))[:limit]
     if query:
-        results = [result for result in results if query in result[0]]
+        logger.debug(query)
+        results = [result for result in results if query in (r.lower() for r in result[0])]  # case insensitive search
     results = [(finder.ngram_fd[result[0]],) + result for result in results]  # get freq for each ngram
     for i in range(len(results)):
         result = list(results[i])
--- web/collocations/views.py
+++ web/collocations/views.py
@@ -12,9 +12,15 @@ class CollocationView(View):
         freq_filter = int(request.GET.get('freq_filter', 1))
         ngram = request.GET.get('ngram')
         assoc_measure = request.GET.get('assoc_measure')
+        window_size = int(request.GET.get('window_size'))
         include_examples = request.GET.get('include_examples')
         if ngram:
-            collocations = get_collocates(ngram, assoc_measure, include_examples, query=query, freq_filter=freq_filter)
+            collocations = get_collocates(ngram,
+                                          assoc_measure,
+                                          include_examples,
+                                          query=query,
+                                          freq_filter=freq_filter,
+                                          window_size=window_size)
             context = {
                 'collocations': collocations,
             }
